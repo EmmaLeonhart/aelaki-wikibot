69443fcfda8025856a411aba14272be6c9bb231f
immanuelle-leonhart <ericrichardbishop@gmail.com>
2025-05-03 20:06:23 -0700

    q

--- Aelaki2 no chatGPT/obj/Debug/net8.0/Aelaki2 no chatGPT.AssemblyInfo.cs
+++ Aelaki2 no chatGPT/obj/Debug/net8.0/Aelaki2 no chatGPT.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aelaki2 no chatGPT")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4c13a4e41eacad21a4ba87b3fae7a5b7146cdeb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aelaki2 no chatGPT")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aelaki2 no chatGPT")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
