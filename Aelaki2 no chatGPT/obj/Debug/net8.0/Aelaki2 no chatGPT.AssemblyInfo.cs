a7f1a352765402da3ed0ccae652bbb55d609d93d
Emma Leonhart <emma@topazcomputing.com>
2025-05-08 23:44:02 -0700

    removed this project, History was unfortunately not properly merged

--- Aelaki2 no chatGPT/obj/Debug/net8.0/Aelaki2 no chatGPT.AssemblyInfo.cs
+++ Aelaki2 no chatGPT/obj/Debug/net8.0/Aelaki2 no chatGPT.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aelaki2 no chatGPT")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4664106ca9be5b993d90a60b6b6ea95c063cf5b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+168599c268d8152e2bf07e17ec685999b3c4d435")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aelaki2 no chatGPT")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aelaki2 no chatGPT")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
