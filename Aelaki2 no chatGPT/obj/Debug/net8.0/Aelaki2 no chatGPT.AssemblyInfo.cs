049e69a247dd4788441348e408698a6322dff54a
immanuelle-leonhart <ericrichardbishop@gmail.com>
2025-05-08 23:46:23 -0700

    merged in this file

--- Aelaki2 no chatGPT/obj/Debug/net8.0/Aelaki2 no chatGPT.AssemblyInfo.cs
+++ Aelaki2 no chatGPT/obj/Debug/net8.0/Aelaki2 no chatGPT.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aelaki2 no chatGPT")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+168599c268d8152e2bf07e17ec685999b3c4d435")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69bf5181096402ad87127626bfec4b8b194118d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aelaki2 no chatGPT")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aelaki2 no chatGPT")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
