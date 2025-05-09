8fb7413583a6ee8bbfbf8c98195fe1699ae8fff1
Emma Leonhart <emma@topazcomputing.com>
2025-05-08 20:20:39 -0700

    qq

--- Verb/obj/Debug/net8.0/Verb.AssemblyInfo.cs
+++ Verb/obj/Debug/net8.0/Verb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Verb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ea9f37a14d06abb9e1458a8f24580b2199dfc2d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7987d48fcb1a5c1905306c7b48c2ab7475634e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Verb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Verb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
